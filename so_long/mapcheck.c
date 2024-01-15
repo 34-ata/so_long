b40c25ff9c6602c27b56de68075b90c2f3e30abe
Fatih Mehmet Ata <faata@k1m03s01.42istanbul.com.tr>
2024-01-15 16:22:52 +0300

    .

--- so_long/mapcheck.c
+++ so_long/mapcheck.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mapcheck.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: faata <faata@student.42.fr>                +#+  +:+       +#+        */
+/*   By: faata <faata@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:50:55 by faata             #+#    #+#             */
-/*   Updated: 2023/12/04 17:53:52 by faata            ###   ########.fr       */
+/*   Updated: 2023/12/04 17:53:52 by faata            ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,8 @@ int	boundary(t_mlx *st_mlx)
 	while (st_mlx->map[i])
 		if (st_mlx->map[i++][0] != '1')
 			return (0);
-	j = ft_strlen(st_mlx->map[0]) - 1;
+	i = 0;
+	j = ft_strlen(st_mlx->map[0]) - 2;
 	while (st_mlx->map[i])
 		if (st_mlx->map[i++][j] != '1')
 			return (0);
@@ -68,25 +69,25 @@ int	boundary(t_mlx *st_mlx)
 
 int	ft_count(t_mlx *st_mlx, int exit_count, int cookie_count, int monster_count)
 {
-	int	i;
-	int	j;
-
-	i = -1;
-	while (st_mlx->map[++i])
+	st_mlx->i = -1;
+	while (st_mlx->map[++st_mlx->i])
 	{
-		j = -1;
-		while (st_mlx->map[i][++j])
+		st_mlx->j = -1;
+		while (st_mlx->map[st_mlx->i][++st_mlx->j])
 		{
-			if (st_mlx->map[i][j] == 'C')
+			if (st_mlx->map[st_mlx->i][st_mlx->j] == 'C')
 				cookie_count += 1;
-			else if (st_mlx->map[i][j] == 'E')
+			else if (st_mlx->map[st_mlx->i][st_mlx->j] == 'E')
 				exit_count += 1;
-			else if (st_mlx->map[i][j] == 'P' && (monster_count++, 1))
+			else if (st_mlx->map[st_mlx->i][st_mlx->j] == 'P'
+				&& (monster_count++, 1))
 			{
-				st_mlx->monster_x = j;
-				st_mlx->monster_y = i;
+				st_mlx->monster_x = st_mlx->j;
+				st_mlx->monster_y = st_mlx->i;
 			}
-			else if (st_mlx->map[i][j] != '\n' && st_mlx->map[i][j] != '1' && st_mlx->map[i][j] != '0')
+			else if (st_mlx->map[st_mlx->i][st_mlx->j] != '\n'
+				&& st_mlx->map[st_mlx->i][st_mlx->j] != '1'
+					&& st_mlx->map[st_mlx->i][st_mlx->j] != '0')
 				ft_close("Invalid map element(s).\n");
 		}
 	}
@@ -99,10 +100,10 @@ void	map_check(t_mlx *st_mlx)
 {
 	if (!ft_count(st_mlx, 0, 0, 0))
 		ft_close("Incorrect cookie, player or exit count.\n");
-	if (!boundary(st_mlx))
-		ft_close("Map does not surround by walls.\n");
 	if (!overload(st_mlx))
 		ft_close("Map lines are not equal.\n");
+	if (!boundary(st_mlx))
+		ft_close("Map does not surround by walls.\n");
 	if (!(flood_fill(st_mlx) && flood_fill0(st_mlx)))
 		ft_close("Map has unreachable element(s).\n");
 	if (!ft_extension(st_mlx->map_path))
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -1,40 +1,55 @@
-#ifndef SO_LONG
-#define SO_LONG
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faata <faata@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 16:50:37 by faata             #+#    #+#             */
+/*   Updated: 2024/01/15 13:21:36 by faata            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include "mlx/mlx.h"
-#include "get_next_line.h"
-#include <fcntl.h>
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
-typedef struct t_mlx {
-	void *mlx;
-	void *mlx_win;
-	void *monster;
-	void *cookie;
-	void *wall;
-	void *parquet;
-	void *exit;
-	char **map;
-	char *map_path;
-	int count_cookie;
-	int monster_x;
-	int monster_y;
-	int map_x;
-	int map_y;
-	int num_of_mov;
-} t_mlx;
+# include <stdlib.h>
+# include <unistd.h>
+# include "get_next_line.h"
+# include <fcntl.h>
+# include "mlx/mlx.h"
 
-void map_check(t_mlx *st_mlx);
-void flood_fill3(char **map, int x, int y, t_mlx *st_mlx);
-void flood_fill2(char **map, int x, int y, t_mlx *st_mlx);
-int flood_fill(t_mlx *st_mlx);
-int flood_fill0(t_mlx *st_mlx);
-int	key_hook(int keycode, t_mlx *vars);
+typedef struct t_mlx
+{
+	void	*mlx;
+	void	*mlx_win;
+	void	*monster;
+	void	*cookie;
+	void	*wall;
+	void	*parquet;
+	void	*exit;
+	char	**map;
+	char	*map_path;
+	int		count_cookie;
+	int		monster_x;
+	int		monster_y;
+	int		map_x;
+	int		map_y;
+	int		num_of_mov;
+	int		i;
+	int		j;
+}	t_mlx;
+
+void	map_check(t_mlx *st_mlx);
+void	flood_fill3(char **map, int x, int y, t_mlx *st_mlx);
+void	flood_fill2(char **map, int x, int y, t_mlx *st_mlx);
+int		flood_fill(t_mlx *st_mlx);
+int		flood_fill0(t_mlx *st_mlx);
+int		key_hook(int keycode, t_mlx *vars);
 void	ft_printmap(t_mlx *st_mlx, int i, int x, int y);
-int ft_maplen(char *path);
-char **ft_getmap(char *path);
+int		ft_maplen(char *path);
+char	**ft_getmap(char *path);
 void	ft_putnbr(unsigned int a);
-int	ft_close(char *x);
+int		ft_close(char *x);
 
 #endif
